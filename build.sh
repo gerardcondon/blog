26fee0faaecc79dd3497b52cf8f0a218cb373f30
gerardcondon <gercondon@gmail.com>
2019-11-13 12:38:49 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
 
 git submodule init
-git submodule update
+git submodule update --remote
 
 cd website
 bundle
